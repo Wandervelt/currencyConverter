51daa951a9d4bd25057f658f52d47b6daf5d8506
Haitham <haithamshami1@gmail.com>
2021-10-07 17:40:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function App() {
+  const [baseCurrency, setBaseCurrency] = useState("usd");
+  const [convertToCurrency, setConvertToCurrency] = useState("eur");
+  const [currencyList, setCurrencyList] = useState([]);
+  const [dropdownList, setDropdownList] = useState([]);
+  const [amountToConvert, setAmountToConvert] = useState(1);
+  const [convertedValue, setConvertedValue] = useState(0);
+  const [hasAnError, setHasAnError] = useState(false);
+
+  useEffect(() => {
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${baseCurrency}.json`
+    )
+      .then((res) => {
+        setCurrencyList(res.data[baseCurrency]);
+      })
+      .then(() => {
+        setConvertedValue(amountToConvert * currencyList[convertToCurrency]);
+      });
+  }, [baseCurrency, amountToConvert, convertToCurrency, currencyList]);
+
+  useEffect(() => {
+    setDropdownList(Object.keys(currencyList).map((x) => x.toUpperCase()));
+  }, [currencyList]);
+
+  const baseCurrencyChangeHandler = (event) => {
+    setBaseCurrency(event.value.toLowerCase());
+  };
+  const convertedCurrencyChangeHandler = (event) => {
+    setConvertToCurrency(event.value.toLowerCase());
+  };
+  const currencyConversionAmountHandler = (event) => {
+    setAmountToConvert(event.target.value);
+  };
+  const swapHandler = () => {
+    let temValue = baseCurrency;
+    setBaseCurrency(convertToCurrency);
+    setConvertToCurrency(temValue);
+  };
+
+  useEffect(() => {
+    setHasAnError(isNaN(amountToConvert));
+  }, [amountToConvert]);
+
+  const amountInputClasses = hasAnError ? "amount invalid" : "amount";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="card">
+      <div className="title">Currency Converter</div>
+      <div className="conversion">
+        <div>
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            className={amountInputClasses}
+            value={amountToConvert}
+            type="text"
+            placeholder="Enter Amount To Convert"
+            onChange={currencyConversionAmountHandler}
+          />
+          {hasAnError && <p className="error-text">Please use numbers only.</p>}
+        </div>
+        <div className="dropdown-swap">
+          <div>
+            <label>From</label>
+            <Dropdown
+              className="dropdown"
+              options={dropdownList}
+              placeholder="from"
+              value={baseCurrency.toUpperCase()}
+              onChange={baseCurrencyChangeHandler}
+            />
+          </div>
+          <div>
+            <button className="button" onClick={swapHandler}>
+              <FontAwesomeIcon icon={faExchangeAlt} />
+            </button>
+          </div>
+          <div>
+            <label>To</label>
+            <Dropdown
+              className="dropdown"
+              options={dropdownList}
+              placeholder="to"
+              value={convertToCurrency.toUpperCase()}
+              onChange={convertedCurrencyChangeHandler}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="value">
+        <p className="currencyToConvert">{`${amountToConvert} ${baseCurrency.toUpperCase()} = `}</p>
+        <p className="convertedCurrency">
+          {`${parseFloat(convertedValue).toFixed(
+            2
+          )} ${convertToCurrency.toUpperCase()}`}{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
